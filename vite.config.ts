c48aa9f47e1fb854b0df9bafc5aab114a06764eb
Blaszka <mateuszniema1@gmail.com>
2025-03-29 12:24:27 +0100

    312231

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import path from "path";
-import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-  return {
-    base: '/DomekLukas/', // Poprawione: Dodane return aby konfiguracja została zwrócona
-    server: {
-      host: "::",
-      port: 8080,
+export default defineConfig({
+  base: '/DomekLukas/', // Nazwa repozytorium
+  plugins: [react()],
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
     },
-    plugins: [
-      react(),
-      mode === 'development' && componentTagger(),
-    ].filter(Boolean),
-    resolve: {
-      alias: {
-        "@": path.resolve(__dirname, "./src"),
-      },
-    },
-    build: {
-      outDir: 'dist', // Upewnij się, że output jest kierowany do folderu "dist"
-    },
-  };
+  },
+  build: {
+    outDir: 'dist', // Folder wynikowy
+  },
 });
